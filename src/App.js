ef0050c6bb5c291971baefe73ec37656dfe54620
ChadlinMaalgraaff <cmaalgraaff010799@gmail.com>
2020-05-20 02:33:35 +0200

    person component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Feed from './Components/Dashboard/feed';
 import Chat from './Components/Messaging/chat';
+import Person from './Components/Messaging/person';
 
 function App() {
     return ( 
       <div className = "App" style={{margin:'0px', padding:'0px', width:'100%'}}>
-        <Chat style={{width:'100%'}}/>
+        <Person style={{width:'100%'}}/>
       </div>
     );
 }
---
+++ src/Components/Messaging/person.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import { Container, Row, Col, Image } from "react-bootstrap";
+import './messaging.css';
+import pp from '../../Images/pp1.jpg';
+
+class Person extends Component {
+  state = {
+    personId: this.props.personId,
+    personName: this.props.personName,
+    personPP: this.props.personPP
+  };
+
+  render() {
+    return (
+      <Container fluid>
+        <Row className='person'>
+            <Col
+                xs={3}
+                sm={3}
+                md={1}
+                lg={1}
+                xl={1}
+            >
+                <Image src={pp} className='message-pp'></Image>
+            </Col>
+            <Col
+                xs={9}
+                sm={9}
+                md={11}
+                lg={11}
+                xl={11}
+            >
+                <div style={{display:'inline-block'}}>
+                <p
+                    style={{marginBottom:'0px', fontSize:'20px', marginLeft:'0px'}}
+                >
+                    Chadlin Maalgraaff
+                </p>
+                </div>
+            </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Person;
