e24b9fc7ed16d2c176bd8e5d88cc869cc35fb9e7
Zee <gebengazizipho@gmail.com>
2020-06-07 22:01:37 +0200

    last

--- src/Components/Profile/friends.js
+++ src/Components/Profile/friends.js
@@ -4,6 +4,7 @@ import { Card, Container, Row, Col, Modal, Button, Form,Image } from "react-boot
 import pp1 from '../../Images/pp1.jpg';
 import pp2 from '../../Images/pp2.jpg';
 import { Avatar } from '@material-ui/core';
+import axios from 'axios';
 
 
 
@@ -16,49 +17,112 @@ class Friends extends Component{
     this.state = { 
       show:false,
         usernames: [
-            {
-                name:'Fay-yaaz',
-                avatar: pp1, 
-                id: 1
-             }, 
-             {
-                 name: 'Thauren',
-                 avatar: pp2 ,
-                 id:2
-             }, 
-             {
-                 name: 'Barbra',
-                 avatar: pp1,
-                 id:3
-             }, 
-             {
-                 name: 'Zee',
-                 avatar: pp2 ,
-                 id:4
-             }, 
-             {
-                 name: 'Chadlin',
-                 avatar: pp1 ,
-                 id:5
-     
-             }
+           
          ],
          followerIDs: [1,2,3,4,5],
          profilePersonId: 6,
-         loggedInPerson: 'Zizipho'
+         loggedInPerson: '',
+         selectedFriend: '',
+         Isfriend:[]
 
     
     }
   }
+componentDidMount(){
+  const options = {
+    headers: {
+      'Content-Type' : 'application/json',
+      'Authorization': 'Token ' + localStorage.getItem('token') 
+    }
+  }
+  axios.get('http://3.209.12.36:8000/api/account/properties',options)
+  .then((res) =>{
+    this.setState({
+      loggedInPerson:res.data.first_name
+    })
+  })
+
+  axios.post('http://3.209.12.36:8000/api/account/friends_list',{'user':localStorage.getItem('user')},options)
+  .then((res) => {
+    console.log("Response: got friends list",res)
+    this.setState({
+      friends:res.data.count,
+      usernames:res.data.friends
+
+    })
+  })
+  .catch((err) => {
+    console.log("couldn't get friends list",err)
+    console.log("state user",this.state.user)
+  })
+}
+
+sendRequest = index => {
+  const username = '';
+  const id = this.state.usernames.map((username,j) => username.pk) 
+  const to_id = id[index]
+
+  const options = {
+    headers: {
+      'Content-Type' : 'application/json',
+      'Authorization': 'Token ' + localStorage.getItem('token') 
+    }
+  }
+  axios.post('http://3.209.12.36:8000/api/account/send_request',{to_user:to_id,from_user:localStorage.getItem('id')},options)
+  .then((res) => {
+    console.log("Response: request sent",res)
+    
+      this.setState(state => {
+        const usernames = this.state.usernames.filter((item, j) => index != j);
+        this.setState({show:false})
+        return {
+            usernames,
+        };
+
+      })
+  })
+  .catch((err) => {
+    console.log("couldn't send request",err)
+  })
+
+}
 
 removeItem = index => {
-  this.setState(state => {
+  const username = '';
+  const id = this.state.usernames.map((username,j) => username.pk) 
+  const to_id = id[index]
+
+  const options = {
+    headers: {
+      'Content-Type' : 'application/json',
+      'Authorization': 'Token ' + localStorage.getItem('token') 
+    }
+  }
+
+  console.log("selected friend",to_id)
+
+
+  axios.post('http://3.209.12.36:8000/api/account/unfriend',{to_user:to_id,from_user:localStorage.getItem('id')},options)
+  .then((res) => {
+    console.log("Response: unfriended",res)
+    this.setState({
+      show:false,
+      Isfriend:false,
+
+    })
+  })
+  .catch((err) => {
+    console.log("couldn't unfriend",err)
+  })
+
+
+  /*this.setState(state => {
       const usernames = this.state.usernames.filter((item, j) => index != j);
       this.setState({show:false})
       return {
           usernames,
       };
-  });
+  });*/
 }; 
 
 
@@ -91,28 +155,42 @@ return (
         <div className= ' text-center' style={{fontFamily:'Vision-Heavy', fontSize:'30px',fontWeight:'bold'}}>
                 {this.state.loggedInPerson}'s Friends
             </div>
-        {this.state.usernames.map((username, index) => (
+        {this.state.usernames.map((username,index) => (
+          
         <Container maxWidth='xs' fixed>
             <Card width='8rem'  >
                 <Card.Body className = 'cont' >
                     <Row  >
                         <Col md="auto">
-                            <Avatar alt="person" src={username.avatar} />
+                            <Avatar alt="person" src={'https://www.gravatar.com/avatar/'+username.avatar+'?s=500'}/>
+
                         </Col>
                         <Col style={{alignContent: "center"}}>
-                            @{username.name}
-                        </Col>
+                        <div
+                          style={{ display:'inline'}}
+                        >
+                            {username.first_name} {username.last_name}
+                       
+                        
+                        <div style= {{fontSize:'13px', color:'grey'}}>
+                         @{username.username}
+                         </div>
+                      </div>
+                      </Col>
                         <Col> 
-                            
-                            <Button className='myButton' onClick ={()=> this.setState({show:true})} >
-                                        Unfriend
-                            </Button>
-                            
+                            <div>{this.state.Isfriend ?
+                            <Button className='myButton' onClick ={() => this.removeItem(index)} >
+                              Unfriend
+                            </Button> :
+                            <Button className= 'myButton1' onClick ={()=> this.sendRequest(index)}>
+                              Send request
+                              </Button>}
+                            </div>
                           <Modal show={this.state.show} onHide={handleClose}>
                             <Modal.Header closeButton>
                               <Modal.Title>Unfriend</Modal.Title>
                             </Modal.Header>
-                            <Modal.Body>Are you sure you want to Unfriend? {username.name}</Modal.Body>
+                            <Modal.Body>Are you sure you want to Unfriend? {username.first_name}</Modal.Body>
                             <Modal.Footer>
                               <Button variant="secondary" onClick={handleClose}>
                                 Cancel
